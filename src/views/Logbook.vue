b816f735fdf7b7aa192e57b7406e84ce7cf87d86
James Patrick Gibson <james@jamespatrickgibson.com>
2019-07-15 15:37:05 -0400

    Transitions in place, and updated back/forward buttons with icons

--- src/views/Logbook.vue
+++ src/views/Logbook.vue
@@ -3,28 +3,6 @@
     <transition :name="transitionName">
       <router-view class="child-view"/>
     </transition>
-    <!-- All Jumps -->
-    <section class="all-jumps" v-show="false">
-      <ol class="jumps">
-        <li v-for="jump in jumps" :key="jump.id" class="jump">
-          <p class="jump__number">{{ jump.jumpNumber }}</p>
-          <div class="jump__overview">
-            <p class="jump__type"><span class="jump__jumper-count">{{ jumperCountName(jump.jumperCount) }}</span> {{ jumpTypeName(jump.jumpType) }}</p>
-            <p class="jump__location">{{ jump.location }}</p>
-            <p class="jump__date">{{ jumpDate(jump.date) }}</p>
-            <!--<p>Aircraft: {{ jump.aircraft }}</p> -->
-            <!--<p>Altitude: {{ jump.exitAltitude.toLocaleString() }}</p>-->
-            <!--<p>Delay: {{ jump.freefallDelay }}s</p>-->
-            <!--<p>Notes: {{ jump.notes }}</p>-->
-            <!--
-            <div class="t-buttons">
-              <button class="t-button" @click="deleteJump(jump.id)">Delete</button>
-            </div>
-            -->
-          </div>
-        </li>
-      </ol>
-    </section>
   </div>
 </template>
 
@@ -46,29 +24,6 @@ export default {
       const fromDepth = from.path.split('/').length
       this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
     }
-  },
-  methods: {
-    deleteJump (id) {
-      this.$store.commit('deleteJump', id)
-    },
-    jumpDate (d) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' }
-      const formattedDate = new Date(d).toLocaleDateString('en-US', options)
-      return formattedDate
-    },
-    jumperCountName (count) {
-      let name = this.jumperCountOptions.find(o => o.value === count).text
-      return name
-    },
-    jumpTypeName (value) {
-      let name = this.jumpTypeOptions.find(o => o.value === value).text
-      return name
-    }
-  },
-  computed: {
-    jumps () {
-      return this.$store.state.jumps.slice(0).sort((b, a) => parseFloat(a.jumpNumber) - parseFloat(b.jumpNumber))
-    }
   }
 }
 </script>
@@ -79,59 +34,6 @@ export default {
   @include desktop() {
     padding-left: 1rem;
     padding-right: 1rem;
-  }
-}
-
-// All Jumps
-.all-jumps {
-  .t-large-title {
-    margin-bottom: 1rem;
-    margin-left: 0.5rem;
-    margin-right: 0.5rem;
-  }
-}
-// Jump List
-.jumps {
-  list-style-type: none;
-  padding: 0;
-}
-.jump {
-  background: #fff;
-  color: $text-light;
-  border-bottom: px-rem(2px) solid $blue-grey-050;
-  display: flex;
-
-  &__number {
-    display: flex;
-    justify-content: center;
-    align-self: stretch;
-    align-items: center;
-    flex: 0 0 5rem;
-    font-size: px-rem(28px);
-    font-weight: bold;
-    color: $blue-grey-700;
-    background: $blue-grey-025;
-    @include desktop {
-      flex: 0 0 6rem;
-    }
-  }
-  &__overview {
-    padding: $space-s;
-  }
-  &__type {
-    @include title-2;
-  }
-  &__location {
-    @include subhead;
-    color: $muted;
-    font-weight: bold;
-    margin-bottom: $space-s;
-  }
-  &__date {
-    @include caption-2();
-    color: $blue-grey-500;
-    margin-bottom: $space-xs;
-    text-transform: uppercase;
   }
 }
 .child-view {
--- src/views/LogbookDetail.vue
+++ src/views/LogbookDetail.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="logbook-detail">
-    <router-link to="/">Back</router-link>
-    <h1>Logbook Detail</h1>
+    <router-link to="/" tag="button" class="t-button has-icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="butt" stroke-linejoin="bevel"><path d="M15 18l-6-6 6-6"/></svg>
+      Back
+    </router-link>
+    <h1 class="t-large-title">Solo Wingsuit</h1>
   </section>
 </template>
 
@@ -14,6 +17,10 @@ export default {
 <style lang="scss" scoped>
 .logbook-detail {
   height: 100vh;
-  background: lemonchiffon;
+  padding: $space-m;
+  .t-button.has-icon {
+    line-height: 1.5;
+    margin-bottom: $space-l;
+  }
 }
 </style>
