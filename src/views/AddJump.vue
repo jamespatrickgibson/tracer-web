10cc62f4ab8ca6937a457271f59be923308965e7
James Patrick Gibson <james@jamespatrickgibson.com>
2019-07-15 11:11:10 -0400

    Added new object maps for selects on Add Jump screen

--- src/views/AddJump.vue
+++ src/views/AddJump.vue
@@ -19,7 +19,7 @@
         <div class="t-field">
           <label class="t-label">Location</label>
           <div class="control">
-            <input class="t-input" type="text" name="dropzones" list="dropzones" v-model="jumpData.location"/>
+            <input class="t-input" type="text" name="dropzones" list="dropzones" v-model="jumpData.location" placeholder="Dropzone name"/>
             <datalist id="dropzones">
               <option>Skydive CNY</option>
               <option>Skydive The Ranch</option>
@@ -41,6 +41,12 @@
           <label class="t-label">Jump Type</label>
           <div class="control">
             <select class="t-select" v-model="jumpData.jumpType">
+              <option v-for="option in jumpTypeOptions" :key="option.value" v-bind:value="option.value">
+                {{ option.text }}
+              </option>
+            </select>
+            <!--
+            <select class="t-select" v-model="jumpData.jumpType">
               <option disabled value="">Select a Jump Type</option>
               <option>Formation Skydive</option>
               <option>Freefly</option>
@@ -50,6 +56,7 @@
               <option>Vertical Formation</option>
               <option>Wingsuit</option>
             </select>
+            -->
           </div>
         </div>
 
@@ -58,16 +65,9 @@
           <label class="t-label">Jumper Count</label>
           <div class="control">
             <select class="t-select" v-model="jumpData.jumperCount">
-              <option disabled value="">Jumper Count</option>
-              <option>Solo</option>
-              <option>2 Way</option>
-              <option>3 Way</option>
-              <option>4 Way</option>
-              <option>5 Way</option>
-              <option>6 Way</option>
-              <option>7 Way</option>
-              <option>8 Way</option>
-              <option>9+ Way</option>
+              <option v-for="option in jumperCountOptions" :key="option.value" v-bind:value="option.value">
+                {{ option.text }}
+              </option>
             </select>
           </div>
         </div>
@@ -93,26 +93,56 @@ export default {
   data () {
     return {
       jumpData: {
-        id: null,
-        jumpNumber: null,
+        id: 1,
+        jumpNumber: 1,
         date: new Date().toISOString(),
         location: '',
-        exitAltitude: 10000,
-        notes: null,
-        jumperCount: '',
-        jumpType: ''
-      }
+        aircraft: '',
+        exitAltitude: 13500,
+        deploymentAltitude: 4500,
+        freefallTime: 60,
+        cutaway: false,
+        jumperCount: 1,
+        jumpType: 'fs',
+        notes: null
+      },
+      jumpTypeOptions: [
+        { text: 'Formation Skydive', value: "fs" },
+        { text: 'Freefly', value: "ff" },
+        { text: 'Tracking', value: "tr" },
+        { text: 'Hop and Pop', value: "hp" },
+        { text: 'CReW', value: "cr" },
+        { text: 'Vertical Formation', value: "vfs" },
+        { text: 'Wingsuit', value: "ws" }
+      ],
+      jumperCountOptions: [
+        { text: 'Solo',   value: "1" },
+        { text: '2 Way',  value: "2" },
+        { text: '3 Way',  value: "3" },
+        { text: '4 Way',  value: "4" },
+        { text: '5 Way',  value: "5" },
+        { text: '6 Way',  value: "6" },
+        { text: '7 Way',  value: "7" },
+        { text: '8 Way',  value: "8" },
+        { text: '9+ Way', value: "9" }
+      ]
     }
   },
   mounted () {
     let lastJump = this.$store.state.jumps.slice(0).sort((b, a) => parseFloat(a.jumpNumber) - parseFloat(b.jumpNumber))[0]
-    this.jumpData.id = lastJump.id + 1
-    this.jumpData.jumpNumber = lastJump.jumpNumber + 1
-    this.jumpData.location = lastJump.location
-    this.jumpData.exitAltitude = lastJump.exitAltitude
-    this.jumpData.notes = lastJump.notes
-    this.jumpData.jumperCount = lastJump.jumperCount
-    this.jumpData.jumpType = lastJump.jumpType
+    if (lastJump != null) {
+      this.jumpData.id = lastJump.id + 1
+      this.jumpData.jumpNumber = lastJump.jumpNumber + 1
+      this.jumpData.location = lastJump.location
+      this.jumpData.aircraft = lastJump.aircraft
+      this.jumpData.exitAltitude = lastJump.exitAltitude
+      this.jumpData.deploymentAltitude = lastJump.deploymentAltitude
+      this.jumpData.freefallTime = lastJump.freefallTime
+      this.jumpData.cutaway = lastJump.cutaway
+      this.jumpData.jumperCount = lastJump.jumperCount
+      this.jumpData.jumpType = lastJump.jumpType
+      this.jumpData.notes = lastJump.notes
+    }
   },
   methods: {
     addJump () {
