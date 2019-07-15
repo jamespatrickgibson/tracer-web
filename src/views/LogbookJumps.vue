596c297d2f226488e4e3462a379094275aa50cbe
James Patrick Gibson <james@jamespatrickgibson.com>
2019-07-15 15:19:47 -0400

    Forgot to add jumps template

---
+++ src/views/LogbookJumps.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="logbook-jumps">
+    <h1>Logbook Jumps</h1>
+    <router-link to="/jumps/detail">Detail</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'logbook-detail'
+}
+</script>
+
+<style lang="scss" scoped>
+.logbook-jumps {
+  height: 100vh;
+  background: tomato;
+}
+</style>
